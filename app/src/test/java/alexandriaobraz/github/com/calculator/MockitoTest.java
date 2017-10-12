a66db15b07b87920497870b0bdf67a66ec18facd
Abrazova Alesia <Obrazova_1998@mail.ru>
2017-10-13 01:05:34 +0300

    unitTest and instrumentationTest changes

--- app/src/test/java/alexandriaobraz/github/com/calculator/MockitoTest.java
+++ app/src/test/java/alexandriaobraz/github/com/calculator/MockitoTest.java
@@ -1,14 +1,14 @@
 package alexandriaobraz.github.com.calculator;
 
-import android.content.Context;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.stub;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -23,6 +23,23 @@ public class MockitoTest {
     public void SetupMockito(){
         mMockCalculator= mock(Calculator.class);
         mSpyCalculator= spy (new Calculator());
+    }
+
+    @Test
+    public void mockTest(){
+        when(mMockCalculator.sum(1, 2)).thenReturn(3.0);
+        when(mMockCalculator.difference(2,1)).thenReturn(1.0);
+        when(mMockCalculator.division(-1,1)).thenReturn(-1.0);
+        when(mMockCalculator.multiply(anyInt(),eq(0))).thenReturn(0.0);
+    }
+
+    @Test
+    public void spyTest(){
+        assertSame(22,mSpyCalculator.sum(20,2));
+        assertSame(180,mSpyCalculator.difference(226,46));
+        assertSame(mSpyCalculator.multiply(98,0),0);
+        mSpyCalculator.division(66,2);
+
     }
 
     @Test
--- app/src/test/java/alexandriaobraz/github/com/calculator/RoboelectricTest.java
+++ app/src/test/java/alexandriaobraz/github/com/calculator/RoboelectricTest.java
@@ -11,27 +11,26 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+
 import org.robolectric.android.controller.ActivityController;
 import org.robolectric.annotation.Config;
 
 import static junit.framework.Assert.assertEquals;
-import static org.mockito.Matchers.anyString;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-@RunWith(RoboelectricTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, sdk=26)
 public class RoboelectricTest {
 
     private ICalculator mMockCalculator;
-    private ICalculator mSpyCalculator;
 
     private ActivityController<CalculatorActivity> activityControl;
-    private EditText inputDigit1;
-    private EditText inputDigit2;
-
     private CalculatorActivity calculatorActivity;
 
     @Before
@@ -43,36 +42,45 @@ public class RoboelectricTest {
         calculatorActivity = activityControl.get();
 
         mMockCalculator = mock(Calculator.class);
-        mSpyCalculator = spy(new Calculator());
-
-        inputDigit1 = (EditText) calculatorActivity.findViewById(R.id.input_first_digit);
-        inputDigit2 = (EditText) calculatorActivity.findViewById(R.id.input_second_digit);
-
-
     }
 
     @Test
     public void mockTest(){
-        inputDigit1.setText(anyString());
-        inputDigit2.setText(anyD());
-        double value1 = Double.parseDouble(inputDigit1.getText().toString());
-        double value2 = Double.parseDouble(inputDigit2.getText().toString());
 
-        when(mMockCalculator.sum(value1, value2)).thenReturn(10);
+        when(mMockCalculator.division(anyInt(),eq(0))).thenThrow(new ArithmeticException());
         assertEquals(mMockCalculator.sum(3, 10), 10);
         verify(mMockCalculator).sum(3, 10);
     }
 
     @Test
     public void spyTest(){
-                final Button divide = (Button) calculatorActivity.findViewById(R.id.button_div_calculator);
+        final Button divide = (Button) calculatorActivity.findViewById(R.id.button_div_calculator);
         divide.performClick();
 
         final TextView resultText = (TextView)calculatorActivity.findViewById(R.id.result_calculator);
-        final String result = resultText.getText().toString();
+        assertNotNull("",resultText);
 
-        assertEquals(mSpyCalculator.division(8, 2), result);
-        verify(mSpyCalculator).division(8, 2);
+        final EditText mFirstDigit = (EditText) calculatorActivity.findViewById(R.id.input_first_digit);
+        assertNotNull("", mFirstDigit);
+
+        final EditText mSecondDigit= (EditText) calculatorActivity.findViewById(R.id.input_second_digit);
+        assertNotNull("", mSecondDigit);
+
+        final Button equals = (Button) calculatorActivity.findViewById(R.id.button_equals_calculator);
+        divide.performClick();
+
+        boolean isDifferenceButtonEnabled = calculatorActivity.findViewById(R.id.button_difference_calculator).isEnabled();
+        assertEquals(isDifferenceButtonEnabled, true);
+        assertTrue(isDifferenceButtonEnabled);
+
+        boolean isSumButtonEnabled = calculatorActivity.findViewById(R.id.button_sum_calculator).isEnabled();
+        assertEquals(isSumButtonEnabled, true);
+        assertTrue(isSumButtonEnabled);
+
+        boolean isMultiplyButtonEnabled = calculatorActivity.findViewById(R.id.button_multiply_calculator).isEnabled();
+        assertEquals(isMultiplyButtonEnabled, true);
+        assertTrue(isMultiplyButtonEnabled);
+
 
     }
 
